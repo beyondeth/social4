9c583bece38598321ac54fcb6bb3023feeb87c8a
beyondeth <92211569+beyondeth@users.noreply.github.com>
2024-09-02 16:36:17 +0900

    Update route.ts

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -6,6 +6,8 @@ export const { GET, POST } = createRouteHandler({
   router: fileRouter,
 });
 
+export const runtime = "edge";
+
 // // export const runtime = "nodejs";
 
 // export async function GET(request: NextRequest) {
